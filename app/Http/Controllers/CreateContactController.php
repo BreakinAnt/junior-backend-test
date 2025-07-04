63f10d3ddbd046336dd67991f82dcb92cc1e7411
Felipe Carneiro <flipecar97@gmail.com>
2025-07-03 22:40:24 -0300

    Refactor CreateContactController to implement service layer and transaction management for contact creation

--- app/Http/Controllers/CreateContactController.php
+++ app/Http/Controllers/CreateContactController.php
@@ -4,11 +4,43 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\ContactsRequest;
+use App\Services\ContactService;
+use Illuminate\Support\Facades\DB;
 
 class CreateContactController extends Controller
 {
-    public function __invoke(ContactsRequest $contactsRequest)
+    protected ContactService $contactService;
+
+    public function __construct(ContactService $contactService)
+    {
+        $this->contactService = $contactService;
+    }
+
+    public function create()
+    {
+        return view('contacts.create');
+    }    
+    
+    public function store(ContactsRequest $contactsRequest)
     {
         $data = $contactsRequest->validated();
+
+        DB::beginTransaction();
+
+        try {
+            $this->contactService->createContact($data);
+
+            DB::commit();
+
+            return redirect()->route('contacts.create')
+                ->with('success', 'Contact created successfully!')
+                ->setStatusCode(200);
+        } catch (\Exception $e) {
+            DB::rollBack();
+
+            return redirect()->route('contacts.create')
+                ->with('error', 'Error creating contact: ' . $e->getMessage())
+                ->withInput();
+        }
     }
 }
