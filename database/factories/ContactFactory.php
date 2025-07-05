cccc22bcb5be411b8d8c984b8716ab84f1ff4c03
Felipe Carneiro <flipecar97@gmail.com>
2025-07-05 19:35:42 -0300

    Enhance ContactFactory with additional Brazilian area codes and update phone generation logic; add ContactSeeder for seeding contacts

--- database/factories/ContactFactory.php
+++ database/factories/ContactFactory.php
@@ -16,10 +16,45 @@ class ContactFactory extends Factory
      */
     public function definition(): array
     {
+        // Brazilian area codes
+        $areaCodes = [
+            11, 12, 13, 14, 15, 16, 17, 18, 19, // São Paulo
+            21, 22, 24, // Rio de Janeiro
+            27, 28, // Espírito Santo
+            31, 32, 33, 34, 35, 37, 38, // Minas Gerais
+            41, 42, 43, 44, 45, 46, // Paraná
+            47, 48, 49, // Santa Catarina
+            51, 53, 54, 55, // Rio Grande do Sul
+            61, // Distrito Federal
+            62, 64, // Goiás
+            63, // Tocantins
+            65, 66, // Mato Grosso
+            67, // Mato Grosso do Sul
+            68, // Acre
+            69, // Rondônia
+            71, 73, 74, 75, 77, // Bahia
+            79, // Sergipe
+            81, 87, // Pernambuco
+            82, // Alagoas
+            83, // Paraíba
+            84, // Rio Grande do Norte
+            85, 88, // Ceará
+            86, 89, // Piauí
+            91, 93, 94, // Pará
+            92, 97, // Amazonas
+            95, // Roraima
+            96, // Amapá
+            98, 99, // Maranhão
+        ];
+
+        $areaCode = $this->faker->randomElement($areaCodes);
+        
+        $phone = $areaCode . '9' . $this->faker->numerify('########');
+
         return [
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
-            'phone' => $this->faker->phoneNumber,
+            'phone' => $phone,
         ];
     }
 }
---
+++ database/seeders/ContactSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Contact;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ContactSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Create 20 contacts using the factory
+        Contact::factory(20)->create();
+    }
+}
