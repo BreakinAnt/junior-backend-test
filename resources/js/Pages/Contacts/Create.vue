39de1375bb2565fe0db3e4260afd7aad65f367cc
Felipe Carneiro <flipecar97@gmail.com>
2025-07-06 19:14:05 -0300

    Refactor Create.vue to improve error handling display and clean up template structure

--- resources/js/Pages/Contacts/Create.vue
+++ resources/js/Pages/Contacts/Create.vue
@@ -1,23 +1,30 @@
 <template>
+    <div v-if="errors">
+        <ul v-for="(error, index) in errors" :key="index">
+            <li>
+                <p class="text-red-500">{{ error }}</p>
+            </li>
+        </ul>
+    </div>
+    <div v-if="form.wasSuccessful">
+        <p class="text-green-500">Contact created successfully!</p>
+    </div>
     <div>
-        <div v-if="errors">
-            <ul v-for="(error, index) in errors" :key="index">
-                <li>
-                    <p class="text-red-500">{{ error }}</p>
-                </li>
-            </ul>
-        </div>
-        Hello World
         <form @submit.prevent="form.post(route('contacts.store'))">
             <button type="submit" class="btn" :disabled="form.processing">Create Contact</button>
         </form>
     </div>
+    <div>
+        <Link :href="route('contacts.index')" class="btn">
+            Back to Contacts
+        </Link>
+    </div>
 </template>
 <script setup>
-import { useForm } from "@inertiajs/vue3";
+import { Link, useForm } from "@inertiajs/vue3";
 import { route } from "ziggy-js";
 
-defineProps({ errors: Object })
+defineProps({ errors: Object });
 
 const form = useForm({
     name: 'test test',
