8a9b86766df076a79bef320b8bd6b668ae974057
Felipe Carneiro <flipecar97@gmail.com>
2025-07-06 17:57:24 -0300

    Add Create.vue component for contact creation with error handling

---
+++ resources/js/Pages/Contacts/Create.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div v-if="errors">
+            <ul v-for="(error, index) in errors" :key="index">
+                <li>
+                    <p class="text-red-500">{{ error }}</p>
+                </li>
+            </ul>
+        </div>
+        Hello World
+        <form @submit.prevent="form.post(route('contacts.store'))">
+            <button type="submit" class="btn">Create Contact</button>
+        </form>
+    </div>
+</template>
+<script setup>
+import { router, useForm  } from "@inertiajs/vue3";
+import { route } from "ziggy-js";
+
+defineProps({ errors: Object })
+
+const form = useForm({
+    name: 'test test',
+    phone: '91987122338',
+    email: "test@"+Math.random(1, 9999),
+});
+
+</script>
