bada24cdb7ecc4894af187c800e8dbc058be5843
Felipe Carneiro <flipecar97@gmail.com>
2025-07-06 16:54:32 -0300

    Add Tailwindcss

--- resources/js/Pages/Contacts/Index.vue
+++ resources/js/Pages/Contacts/Index.vue
@@ -1,14 +1,17 @@
 <template>
     <div>
         <h1>Contacts</h1>
+        <button @click="route('contacts.create')" class="font-sans">
+            Create Contact
+        </button>
         <ul>
         <li v-for="contact in contacts.data" :key="contact.id">
             {{ contact.name }} - {{ contact.email }}
         </li>
         </ul>
-        <pagination :links="contacts.links" />
+        <!-- <pagination :links="contacts.links" /> -->
     </div>
 </template>
-<script>
+<script setup>
     defineProps({ contacts: Object })
 </script>
